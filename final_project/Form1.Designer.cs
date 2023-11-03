b292abe9a68ad63eee8cac68f4ae84c9475b9da5
ustinova <ustinova@NK-W12-8>
2023-11-03 12:11:02 +0200

    Add project files.

---
+++ final_project/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace final_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbAmazing = new System.Windows.Forms.RadioButton();
+            this.rbGood = new System.Windows.Forms.RadioButton();
+            this.rbNormal = new System.Windows.Forms.RadioButton();
+            this.rbBad = new System.Windows.Forms.RadioButton();
+            this.rbTerrible = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbAmazing
+            // 
+            this.rbAmazing.AutoSize = true;
+            this.rbAmazing.Location = new System.Drawing.Point(53, 61);
+            this.rbAmazing.Name = "rbAmazing";
+            this.rbAmazing.Size = new System.Drawing.Size(65, 17);
+            this.rbAmazing.TabIndex = 0;
+            this.rbAmazing.TabStop = true;
+            this.rbAmazing.Text = "Amazing";
+            this.rbAmazing.UseVisualStyleBackColor = true;
+            // 
+            // rbGood
+            // 
+            this.rbGood.AutoSize = true;
+            this.rbGood.Location = new System.Drawing.Point(53, 84);
+            this.rbGood.Name = "rbGood";
+            this.rbGood.Size = new System.Drawing.Size(51, 17);
+            this.rbGood.TabIndex = 1;
+            this.rbGood.TabStop = true;
+            this.rbGood.Text = "Good";
+            this.rbGood.UseVisualStyleBackColor = true;
+            // 
+            // rbNormal
+            // 
+            this.rbNormal.AutoSize = true;
+            this.rbNormal.Location = new System.Drawing.Point(53, 107);
+            this.rbNormal.Name = "rbNormal";
+            this.rbNormal.Size = new System.Drawing.Size(58, 17);
+            this.rbNormal.TabIndex = 2;
+            this.rbNormal.TabStop = true;
+            this.rbNormal.Text = "Normal";
+            this.rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // rbBad
+            // 
+            this.rbBad.AutoSize = true;
+            this.rbBad.Location = new System.Drawing.Point(53, 130);
+            this.rbBad.Name = "rbBad";
+            this.rbBad.Size = new System.Drawing.Size(44, 17);
+            this.rbBad.TabIndex = 3;
+            this.rbBad.TabStop = true;
+            this.rbBad.Text = "Bad";
+            this.rbBad.UseVisualStyleBackColor = true;
+            // 
+            // rbTerrible
+            // 
+            this.rbTerrible.AutoSize = true;
+            this.rbTerrible.Location = new System.Drawing.Point(53, 153);
+            this.rbTerrible.Name = "rbTerrible";
+            this.rbTerrible.Size = new System.Drawing.Size(60, 17);
+            this.rbTerrible.TabIndex = 4;
+            this.rbTerrible.TabStop = true;
+            this.rbTerrible.Text = "Terrible";
+            this.rbTerrible.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "How do you feel today?";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 186);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(185, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.rbTerrible);
+            this.groupBox1.Controls.Add(this.rbBad);
+            this.groupBox1.Controls.Add(this.rbNormal);
+            this.groupBox1.Controls.Add(this.rbGood);
+            this.groupBox1.Controls.Add(this.rbAmazing);
+            this.groupBox1.Location = new System.Drawing.Point(25, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 267);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 332);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbAmazing;
+        private System.Windows.Forms.RadioButton rbGood;
+        private System.Windows.Forms.RadioButton rbNormal;
+        private System.Windows.Forms.RadioButton rbBad;
+        private System.Windows.Forms.RadioButton rbTerrible;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ final_project/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace final_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string GetSelectedMood()
+        {
+            if (rbAmazing.Checked) return "Amazing";
+            if (rbGood.Checked) return "Good";
+            if (rbNormal.Checked) return "Normal";
+            if (rbBad.Checked) return "Bad";
+            if (rbTerrible.Checked) return "Terrible";
+            return "Unknown";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedMood = GetSelectedMood();
+            DateTime selectedDate = dateTimePicker1.Value;
+            string moodData = $"{selectedDate.ToShortDateString()}, {selectedMood}";
+
+            string csvFilePath = "C:\\Users\\ustinova\\Documents\\your_mood_data.csv";
+
+            if (!File.Exists(csvFilePath))
+            {
+                CreateCsvFileWithHeaders(csvFilePath);
+            }
+
+            using (StreamWriter sw = new StreamWriter(csvFilePath, true))
+            {
+                sw.WriteLine(moodData);
+            }
+
+            MessageBox.Show("Mood data added to the database.", "Success");
+        }
+
+        private void CreateCsvFileWithHeaders(string filePath)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine("Date, Mood");
+            }
+        }
+    }
+}
